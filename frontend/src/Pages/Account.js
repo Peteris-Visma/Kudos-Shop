695bcffadfb035713ea3aecd62b03db03541f595
Rob4ixQ <79448944+Rob4ixQ@users.noreply.github.com>
2021-02-25 09:31:21 +0200

    Update Account.js

--- frontend/src/Pages/Account.js
+++ frontend/src/Pages/Account.js
@@ -10,15 +10,15 @@ import Logo from '../Components/KP.png';
     
 const Account = () => {
     const data = [
-    {Number: 1, Date: '10-02-2021 12:45', Kudos: 12, Description: 'T-Shirt (Eat, Sleep, Code, Repeat)', Status: 'Completed'},
-    {Number: 2,Date: '11-02-2021 09:01', Kudos: 25 , Description: 'Umbrella', Status: 'Completed'},
-    {Number: 3,Date: '12-02-2021 13:22', Kudos: 12 , Description: 'Black T-Shirt', Status: 'Completed'} ,
-    {Number: 4,Date: '13-02-2021 10:06', Kudos: 25 , Description: 'Shopping bag (5x)', Status: 'Completed'},
-    {Number: 5,Date: '14-02-2021 17:17', Kudos: 6 , Description: 'Buff', Status: 'Completed'},
-    {Number: 6,Date: '15-02-2021 20:58', Kudos: 25 , Description: 'Dark blue hoodie', Status: 'Completed'},
-    {Number: 7,Date: '16-02-2021 11:11', Kudos: 6, Description: 'Universal size waterproof bag', Status: 'Completed'},
-    {Number: 8,Date: '17-02-2021 12:30', Kudos: 25, Description: 'Umbrella', Status: 'Completed'},
-    {Number: 9,Date: '18-02-2021 18:47', Kudos: 4, Description: '3D printing', Status: 'Completed'},
+    {Number: 9, Date: '10-02-2021 12:45',Remaining: 591, KudosSpent: 12,Description: 'T-Shirt (Eat, Sleep, Code, Repeat)', Status: 'Completed'},
+    {Number: 8,Date: '11-02-2021 09:01', Remaining: 566 , KudosSpent: 25, Description: 'Umbrella', Status: 'Completed'},
+    {Number: 7,Date: '12-02-2021 13:22', Remaining: 560 , KudosSpent: 6, Description: 'Black T-Shirt', Status: 'Completed'} ,
+    {Number: 6,Date: '13-02-2021 10:06', Remaining: 535 , KudosSpent: 25, Description: 'Shopping bag (5x)', Status: 'Completed'},
+    {Number: 5,Date: '14-02-2021 17:17', Remaining: 529 , KudosSpent: 6, Description: 'Buff', Status: 'Completed'},
+    {Number: 4,Date: '15-02-2021 20:58', Remaining: 504 , KudosSpent: 25, Description: 'Dark blue hoodie', Status: 'Completed'},
+    {Number: 3,Date: '16-02-2021 11:11', Remaining: 499, KudosSpent: 5, Description: 'Universal size waterproof bag', Status: 'Completed'},
+    {Number: 2,Date: '17-02-2021 12:30', Remaining: 467, KudosSpent: 32, Description: 'Umbrella', Status: 'Completed'},
+    {Number: 1,Date: '18-02-2021 18:47', Remaining: 455, KudosSpent: 12, Description: '3D printing', Status: 'Completed'},
         ]
         const user = [
         {Name: 'Roberts', Surname: 'Kartenko', KudosWallet: 567},
@@ -30,7 +30,7 @@ const Account = () => {
             <tr key={index}>
             <th>{Info.Number}</th>
             <th>{Info.Date}</th>
-            <th>{Info.Kudos}</th>
+            <th>{Info.KudosSpent}</th>
             <th>{Info.Description}</th>
             <th>{Info.Status}</th>
             </tr>
@@ -67,7 +67,7 @@ const Account = () => {
             </FrontSide>
 
             <BackSide style={FlippyStyle }>
-                <h4>You have </h4>  457 <img src={Logo} className="logo"/>  
+                <h4>You have </h4>  455 <img src={Logo} className="logo"/>  
             </BackSide>
 
         </Flippy>
@@ -75,7 +75,7 @@ const Account = () => {
     <div id="div2">  
         <ResponsiveContainer width="100%" height={300} >
             <LineChart width={1000} height={400} data={data} margin={{ top: 50, right: 50, bottom: 0, left: 200 }}>
-                <Line type="monotone" dataKey="Kudos" strokeWidth={4} stroke="#ec043c" />
+                <Line type="monotone" dataKey="Remaining" strokeWidth={4} stroke="#ec043c" />
                     <CartesianGrid stroke="#ccc"  />
                         <XAxis dataKey="Date"  />
                         <YAxis  />
@@ -88,7 +88,7 @@ const Account = () => {
             <tr>
                 <th>Number</th>
                 <th>Date</th> 
-                <th>Kudos</th> 
+                <th>Kudos spent</th> 
                 <th>Description</th> 
                 <th>Status</th>   
             </tr>
