215d50a85becc01725e60a0cea9504ae34594afb
Peteris-Visma <peteris.savickis@visma.com>
2021-02-25 22:27:26 +0200

    Purchase requests

---
+++ backend_f/BusinessLayer/Models/Input/PurchaseInputModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DbLayer.Entities;
+
+namespace BusinessLayer.Models.Input
+{
+    public class PurchaseInputModel
+    {
+            public int Id { get; set; }
+            public bool IsApprovedP { get; set; }
+            public DateTime RequestDateP { get; set; }
+            public int UserIdP { get; set; }
+            public User UserP { get; set; }
+            public int ProductIdP { get; set; }
+            public Product ProductP { get; set; }
+            public Purchase ToEntity()
+            {
+                return new Purchase
+                {
+                    Id = Id,
+                    IsApproved = IsApprovedP,
+                    RequestDate = RequestDateP,
+                    UserId = UserIdP,
+                    ProductId = ProductIdP,
+                    Product = ProductP,
+                };
+            }
+    }
+}
---
+++ backend_f/KudoShopBackEnd/Controllers/PurchaseController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using BusinessLayer.Models.Input;
+using BusinessLayer.Services;
+
+namespace KudoShopBackEnd.Controllers
+{
+    public class PurchaseController
+    {
+        [Route("api/[controller]")]
+        [ApiController]
+        [EnableCors("AllowOrigin")]
+        public class TransferRequestController : ControllerBase
+        {
+            [HttpGet]
+            [Route("GetPurchases")]
+
+            public IActionResult GetAllPurchases()
+            {
+                var handler = new PurchaseService();
+                var requests = handler.GetAllPurchases();
+                return Ok(requests);
+            }
+            [HttpPost]
+            [Route("AddPurchase")]
+            public IActionResult AddPurchase(PurchaseInputModel model)
+            {
+                var handler = new PurchaseService();
+                handler.AddPurchase(model);
+                return NoContent();
+            }
+            [HttpDelete]
+            [Route("DeletePurchase")]
+            public void RemovePurchase(int id)
+            {
+                var handler = new PurchaseService();
+                handler.RemovePurchase(id);
+
+            }
+        }
+    }
+}
